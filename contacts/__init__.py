ec800ba2270575666d7340ed183e170ea228d574
Maxwell I. Zimmerman <mizimmer@wustl.edu>
2021-12-01 13:57:57 -0600

    PISA SASA contact counting adding

--- contacts/__init__.py
+++ contacts/__init__.py
@@ -1,1 +1,1 @@
-from .count_contacts import count_contacts_distance, min_dists, pairwise_dists
+from .count_contacts import count_contacts_distance, min_dists, pairwise_dists, count_PISA_contacts
--- contacts/count_contacts.py
+++ contacts/count_contacts.py
@@ -2,8 +2,6 @@ import numpy as np
 import mdtraj as md
 
 
-
-
 def pairwise_dists(struct0, struct1):
     """calculate pairwise distances between atoms in two structures.
 
@@ -114,3 +112,116 @@ def count_contacts_distance(struct0, struct1, cutoff=0.39, mode='residue'):
         contacts_struct0 = atomic_contacts_struct0
         contacts_struct1 = atomic_contacts_struct1
     return contacts_struct0, contacts_struct1
+
+
+small_number = 1E-9
+def _calc_sasa_apo_holo(struct0, struct1, mode='residue', **kwargs):
+    """Calculates the solvent exposure of two structures and their differences when bound.
+
+    Inputs
+    ----------
+    struct0 : md.Trajectory,
+        First structure for calculating SASAs.
+    struct1 : md.Trajectory,
+        Second structure for calculating SASAs.
+    mode : str, choices=['atom', 'residue']
+        Optionally return SASA by atom or residue.
+
+    Returns
+    ----------
+    sasa_struct0_apo : nd.array, shape=(n_atoms,) or (n_residues),
+        The SASA of struct0 in isolation, either by atom or residue.
+    sasa_struct1_apo : nd.array, shape=(n_atoms,) or (n_residues),
+        The SASA of struct1 in isolation, either by atom or residue.
+    sasa_struct0_holo : nd.array, shape=(n_atoms,) or (n_residues),
+        The SASA of struct0 in isolation, either by atom or residue.
+    sasa_struct1_holo : nd.array, shape=(n_atoms,) or (n_residues),
+        The SASA of struct1 in isolation, either by atom or residue.
+    """ 
+    # obtain combined structure (add struct1 to struct0)
+    combined_struct_topol = struct0.topology.copy().join(struct1.topology)
+    combined_struct_xyz = np.concatenate([struct0.xyz, struct1.xyz], axis=1)
+    combined_struct = md.Trajectory(combined_struct_xyz, combined_struct_topol)
+    
+    # only residue mode is currently supported
+    if mode == 'residue':
+        
+        # calculate SASAs
+        sasa_struct0_apo = md.shrake_rupley(
+            struct0, mode='residue', **kwargs)[0] + small_number
+        sasa_struct1_apo = md.shrake_rupley(
+            struct1, mode='residue', **kwargs)[0] + small_number
+        sasa_combined_states = md.shrake_rupley(
+            combined_struct, mode='residue', **kwargs)[0] + small_number
+        
+        # extract struct0 and struct1 from combined structure
+        sasa_struct0_holo = sasa_combined_states[:struct0.n_residues]
+        sasa_struct1_holo = sasa_combined_states[struct0.n_residues:]
+    else:
+        raise
+        
+    # return sasas        
+    return sasa_struct0_apo, sasa_struct0_holo, sasa_struct1_apo, sasa_struct1_holo
+
+
+def sasa_change_fraction(struct0, struct1, **kwargs):
+    """Calculate the percentage SASA change from apo to holo.
+
+    Inputs
+    ----------
+    struct0 : md.Trajectory,
+        First structure for calculating SASAs.
+    struct1 : md.Trajectory,
+        Second structure for calculating SASAs.
+
+    Returns
+    ----------
+    sasa_change_frac_struct0 : nd.array, shape=(n_atoms,) or (n_residues),
+        The fraction change of SASA on struct0 apo v holo.
+    sasa_change_frac_struct1 : nd.array, shape=(n_atoms,) or (n_residues),
+        The fraction change of SASA on struct1 apo v holo.
+    """
+
+    # calculate SASAs
+    sasa_struct0_apo, sasa_struct0_holo, sasa_struct1_apo, sasa_struct1_holo = \
+        calc_sasa_apo_holo(struct0, struct1, **kwargs)
+
+    # calculate fractional change
+    sasa_change_struct0 = sasa_struct0_apo - sasa_struct0_holo
+    sasa_change_struct1 = sasa_struct1_apo - sasa_struct1_holo
+    sasa_change_frac_struct0 = (sasa_change_struct0 / sasa_struct0_apo)
+    sasa_change_frac_struct1 = (sasa_change_struct1 / sasa_struct1_apo)
+    return sasa_change_frac_struct0, sasa_change_frac_struct1
+
+
+def bin_sasa_change(sasas):
+    """Bins SASA values by PDBePISA convention,
+    i.e. 10-20% -> 1, 20-30% -> 2, etc."""
+    sasas *= 10
+    sasas = np.array([int(sasa) for sasa in sasas])
+    return sasas
+
+
+def count_PISA_contacts(struct0, struct1, **kwargs):
+    """Determine strength of residue-residue contacts using PISA conventions.
+    Strength is reported in fraction of SASA change between apo-holo structures.
+
+    Inputs
+    ----------
+    struct0 : md.Trajectory,
+        First structure for calculating SASAs.
+    struct1 : md.Trajectory,
+        Second structure for calculating SASAs.
+
+    Returns
+    ----------
+    PISA_SASA_struct0 : nd.array, shape=(n_residues,),
+        Categorized contacts per residue for struct0.
+    PISA_SASA_struct1 : nd.array, shape=(n_residues,),
+        Categorized contacts per residue for struct1.
+    """
+    sasa_frac_struct0, sasa_frac_struct1 = sasa_change_fraction(
+        struct0, struct1, **kwargs)
+    PISA_SASA_struct0 = bin_sasa_change(sasa_frac_struct0)
+    PISA_SASA_struct1 = bin_sasa_change(sasa_frac_struct1)
+    return PISA_SASA_struct0, PISA_SASA_struct1
