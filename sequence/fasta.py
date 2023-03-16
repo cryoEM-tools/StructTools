f5b737ff815fa5c0eada86016705c0511220de20
Maxwell Zimmerman <mizimmer@wustl.edu>
2023-03-16 10:43:43 -0500

    update fasta load/write

--- sequence/fasta.py
+++ sequence/fasta.py
@@ -1,7 +1,10 @@
+import copy
 import numpy as np
+import os
 
-def write_fasta(filename, seq, seq_title, n_resis=70, append=True):
+def _write_fasta(filename, seq, seq_title, n_resis=70, append=True):
     remaining_seq = copy.copy(seq)
+    header = '>%s\n' % seq_title
     if append:
         with open(filename, "a") as f:
             f.write(header)
@@ -17,3 +20,20 @@ def write_fasta(filename, seq, seq_title, n_resis=70, append=True):
                 remaining_seq = remaining_seq[n_resis:]
             f.write("\n")
     return
+
+def write_fasta(filename, seqs, seq_titles, n_resis=70, overwrite=False):
+
+    if not overwrite:
+        assert not os.path.exists(filename)
+
+    assert len(seqs) == len(seq_titles)
+
+    for n in np.arange(len(seqs)):
+        if n == 0:
+            _write_fasta(
+                filename, seqs[n], seq_titles[n], n_resis=n_resis, append=False)
+        else:
+            _write_fasta(
+                filename, seqs[n], seq_titles[n], n_resis=n_resis, append=True)
+    return
+
